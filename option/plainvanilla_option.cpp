577c653f77e664a7ed6559d186c15c70cb0b0ba1
wbpark91 <wbpark91@gmail.com>
2018-03-21 00:28:57 -0500

    fix Swap function(inherit Derivative)

--- option/plainvanilla_option.cpp
+++ option/plainvanilla_option.cpp
@@ -58,16 +58,9 @@ double PlainVanillaOption::delta() {
 
 /* Swap function */
 void PlainVanillaOption::Swap(PlainVanillaOption& lhs, PlainVanillaOption& rhs) {
+    Derivative::Swap(&lhs, &rhs);
+
     /* Option properties swap */
     std::swap(lhs.strike_, rhs.strike_);
-    std::swap(lhs.t_, rhs.t_);
     std::swap(lhs.type_, rhs.type_);
-    std::swap(lhs.payoff_, rhs.payoff_);
-
-    /* Market Variable swap */
-    std::swap(lhs.mktVar_, rhs.mktVar_);
-    std::swap(lhs.s_, rhs.s_);
-    std::swap(lhs.r_, rhs.r_);
-    std::swap(lhs.div_, rhs.div_);
-    std::swap(lhs.sigma_, rhs.sigma_);
 }
