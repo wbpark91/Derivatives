4365c5fe9439b117a88451f4c9a762b32b214ba1
wbpark91 <wbpark91@gmail.com>
2018-04-13 17:31:33 -0500

    set functions

--- fdmengine/explicit_fdm_contcpn.cpp
+++ fdmengine/explicit_fdm_contcpn.cpp
@@ -10,6 +10,7 @@ ExplicitFDMContCpn::ExplicitFDMContCpn(double spot, double maturity,
             :ExplicitFDM(spot, maturity, imax, jmax, upper, lower),
                 mBarrier(barrier), mFreq(freq) {
     mBarrierIdx = mMesh.findIndex(barrier, 0);
+
     if (mBarrierIdx == -1) {
         std::cout << "Initialization error: cannot find barrier." << std::endl;
         exit(1);
@@ -17,3 +18,10 @@ ExplicitFDMContCpn::ExplicitFDMContCpn(double spot, double maturity,
 }
 
 ExplicitFDMContCpn::~ExplicitFDMContCpn() { }
+
+/* set functions */
+void ExplicitFDMContCpn::setCpnIdx(std::vector<unsigned int> cpnIdx) {
+    mCpnIdx = cpnIdx;
+}
+
+void ExplicitFDMContCpn::setCoupon(double coupon) { mCoupon = coupon; }
