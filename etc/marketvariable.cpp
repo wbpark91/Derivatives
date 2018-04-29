fd0b3a741f91a2032a5fc5fdf0b75ca93d220160
wbpark91 <wbpark91@gmail.com>
2018-04-28 19:12:15 -0500

    end line

--- etc/marketvariable.cpp
+++ etc/marketvariable.cpp
@@ -3,13 +3,13 @@
 
 /* Quanto adjustment */
 MarketVariable::MarketVariable(double spot, Forex forex, double dividend,
-    double corr):spot_(spot), div_(dividend), sigma_(sigma) {
+    double sigma, double corr):spot_(spot), div_(dividend), sigma_(sigma) {
     r_ = forex.mRate - corr * sigma * forex.mVol;
 }
 
 void MarketVariable::print() const {
-    std::cout << "Spot: " << spot_ << " ";
-    std::cout << "Rate(Risk-Neutral): " << r_ << " ";
-    std::cout << "Dividend: " << div_ << " ";
-    std::cout << "Volatility: " << spot_ << " ";
+    std::cout << "Spot: " << spot_ << std::endl;
+    std::cout << "Rate(Risk-Neutral): " << r_ << std::endl;
+    std::cout << "Dividend: " << div_ << std::endl;
+    std::cout << "Volatility: " << spot_ << std::endl;
 }
