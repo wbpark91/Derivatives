2b4115579be88c15b2aba58ac53f3d7d84abd875
wbpark91 <wbpark91@gmail.com>
2018-04-28 19:13:09 -0500

    fix some errors

--- multiasset/multiasset_autocall_payoff.h
+++ multiasset/multiasset_autocall_payoff.h
@@ -5,20 +5,28 @@
 class MAACPayoff {
 public:
     MAACPayoff() { }
-    MAACPayoff(double trigger, double maturity, std::vector<double> ACSchedule);
+    MAACPayoff(double trigger, double maturity, std::vector<double> ACSchedule)
         :mTrigger(trigger), mMaturity(maturity), mACSchedule(ACSchedule) {
-        mAutocall = false;
+        mAutoCall = false;
+        mRateSet = false;
     }
     virtual ~MAACPayoff() { }
 
-    virtual double operator(std::vector<double> spot, double time) = 0;
+    virtual double operator()(std::vector<double> spot, double t) = 0;
+
+    /* set functions */
     void setAutoCall(bool autoCall) { mAutoCall = autoCall; }
+    void setDiscountRate(double rate) { mDiscountRate = rate; mRateSet = true; }
 
     bool getAutoCall() const { return mAutoCall; }
 protected:
     double mTrigger;
     double mMaturity;
     std::vector<double> mACSchedule;
+
+    /* discount rate */
+    double mDiscountRate;
+    bool mRateSet;
 
     /* true if autoacalled */
     bool mAutoCall;
